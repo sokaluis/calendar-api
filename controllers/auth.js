54361716546d7619bc847c683379d2c07dc78a95
Luis Azocar <lazocar.dev@gmail.com>
2022-06-12 13:02:39 -0300

    validate data from request

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,26 @@
 const { response, request } = require("express");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User.model");
 
 // Create User
 const createUser = async (req = request, res = response) => {
+  const { email, password } = req.body;
   try {
-    const user = new User(req.body);
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "User already exists",
+      });
+    }
+    user = new User(req.body);
+
+    // Hash password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
+    // Save user
     await user.save();
 
     res.status(201).json({
@@ -21,14 +37,41 @@ const createUser = async (req = request, res = response) => {
 };
 
 // Login User
-const loginUser = (req = request, res = response) => {
+const loginUser = async (req = request, res = response) => {
   const { email, password } = req.body;
-  res.status(200).json({
-    ok: true,
-    mensaje: "Login",
-    email,
-    password,
-  });
+
+  try {
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "User not found",
+      });
+    }
+
+    // Compare password
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "Password incorrect",
+      });
+    }
+
+    // Generate token
+
+    res.status(200).json({
+      ok: true,
+      mensaje: "Login",
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      mensaje: "Error al iniciar session",
+      error,
+    });
+  }
 };
 
 // Renew Token
