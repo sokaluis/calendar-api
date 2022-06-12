227fb3312856b84af5fa22a8c32e8cb21b94ef84
Luis Azocar <lazocar.dev@gmail.com>
2022-06-12 10:22:02 -0300

    .gitignore

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,18 @@
 const { response, request } = require("express");
+const { validationResult } = require("express-validator");
 
 // Create User
 const createUser = (req = request, res = response) => {
   const { name, email, password } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.array(),
+    });
+  }
 
   res.json({
     ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,23 @@
   Rutas de autenticación
   host + /api/auth
 */
-
 const { Router } = require("express");
-const { createUser, renewToken, loginUser } = require("../controllers/auth");
+const { check } = require("express-validator");
 const router = Router();
+
+const { createUser, renewToken, loginUser } = require("../controllers/auth");
+
+router.post(
+  "/new",
+  [
+    // Multiple middlewares
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+  ],
+  createUser
+); // POST /api/auth/new
 
 router.post("/", loginUser); // POST /api/auth
 
 router.get("/renew", renewToken); // GET /api/auth/renew
-
-router.post("/new", createUser); // POST /api/auth/new
 
 module.exports = router;
