158756aa8204a8f859d89f779900b88025b0768e
Luis Azocar <lazocar.dev@gmail.com>
2022-06-12 13:53:30 -0300

    enable cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dbConnection = require("./database/config");
+const cors = require("cors");
 require("dotenv").config();
 
 // Crear el servidor de express
@@ -7,6 +8,9 @@ const app = express();
 
 // Database
 dbConnection();
+
+// Cors
+app.use(cors());
 
 // Directorio publico
 app.use(express.static("public"));
